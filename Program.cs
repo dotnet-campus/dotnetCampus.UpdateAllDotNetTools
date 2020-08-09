f707d50052edf190084ea24d4f0dadb02cdbd75f
Shaojun Li <i@shaojun.li>
2020-08-09 18:28:17 +0800

    Skip blank package Id and fix on macOS.

--- Program.cs
+++ Program.cs
@@ -43,7 +43,7 @@ namespace dotnetCampus.UpdateAllDotNetTools
 
             line = stringReader.ReadLine();
             var regex = new Regex(@"(\S+)\s+", RegexOptions.Compiled);
-            while (line != null)
+            while (!string.IsNullOrWhiteSpace(line))
             {
                 var match = regex.Match(line);
                 yield return match.Groups[1].Value;
@@ -60,14 +60,19 @@ namespace dotnetCampus.UpdateAllDotNetTools
                 workingDirectory = Environment.CurrentDirectory;
             }
 
+            var indexOfSpace = str.IndexOf(' ');
+            var fileName = indexOfSpace == -1 ? str : str.Substring(0, indexOfSpace);
+            var argument = indexOfSpace == -1 ? "" : str.Substring(indexOfSpace + 1);
+
             var p = new Process
             {
                 StartInfo =
                 {
-                    FileName = "cmd.exe",
+                    FileName = fileName,
+                    Arguments = argument,
                     WorkingDirectory = workingDirectory,
                     UseShellExecute = false, //是否使用操作系统shell启动
-                    RedirectStandardInput = true, //接受来自调用程序的输入信息
+                    //RedirectStandardInput = true, //接受来自调用程序的输入信息
                     RedirectStandardOutput = true, //由调用程序获取输出信息
                     RedirectStandardError = true, //重定向标准错误输出
                     CreateNoWindow = true, //不显示程序窗口
@@ -78,59 +83,12 @@ namespace dotnetCampus.UpdateAllDotNetTools
 
             p.Start(); //启动程序
 
-            //向cmd窗口发送输入信息
-            p.StandardInput.WriteLine(str + "&exit");
-
-            p.StandardInput.AutoFlush = true;
-            //p.StandardInput.WriteLine("exit");
-            //向标准输入写入要执行的命令。这里使用&是批处理命令的符号，表示前面一个命令不管是否执行成功都执行后面(exit)命令，如果不执行exit命令，后面调用ReadToEnd()方法会假死
-            //同类的符号还有&&和||前者表示必须前一个命令执行成功才会执行后面的命令，后者表示必须前一个命令执行失败才会执行后面的命令
-
-            bool exited = false;
-
-            //// 超时
-            //Task.Run(() =>
-            //{
-            //    Task.Delay(TimeSpan.FromMinutes(1)).ContinueWith(_ =>
-            //    {
-            //        if (exited)
-            //        {
-            //            return;
-            //        }
-
-            //        try
-            //        {
-            //            if (!p.HasExited)
-            //            {
-            //                Console.WriteLine($"{str} 超时");
-            //                p.Kill();
-            //            }
-            //        }
-            //        catch (Exception e)
-            //        {
-            //            Console.WriteLine(e);
-            //        }
-            //    });
-            //});
-
-            //获取cmd窗口的输出信息
+            //获取输出信息
             string output = p.StandardOutput.ReadToEnd();
-            //Console.WriteLine(output);
             output += p.StandardError.ReadToEnd();
-            //Console.WriteLine(output);
-
-            //StreamReader reader = p.StandardOutput;
-            //string line=reader.ReadLine();
-            //while (!reader.EndOfStream)
-            //{
-            //    str += line + "  ";
-            //    line = reader.ReadLine();
-            //}
 
             p.WaitForExit(TimeSpan.FromMinutes(1).Milliseconds); //等待程序执行完退出进程
             p.Close();
-
-            exited = true;
 
             return output + "\r\n";
         }
