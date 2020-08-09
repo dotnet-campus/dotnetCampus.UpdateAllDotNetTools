5d7ade1a24ca6a0f554c63a26ac02d8140b6da94
lindexi <lindexi_gd@163.com>
2020-08-09 21:08:02 +0800

    修复进程不返回

--- Program.cs
+++ Program.cs
@@ -52,7 +52,6 @@ namespace dotnetCampus.UpdateAllDotNetTools
             }
         }
 
-
         private static string Command(string fileName, string argument, string workingDirectory = null)
         {
             Console.WriteLine($"{fileName} {argument}");
@@ -80,14 +79,35 @@ namespace dotnetCampus.UpdateAllDotNetTools
 
             p.Start(); //启动程序
 
-            //获取输出信息
-            string output = p.StandardOutput.ReadToEnd();
-            output += p.StandardError.ReadToEnd();
+            const string breakLine = "\r\n";
+            string output = "";
+            p.OutputDataReceived += (sender, args) =>
+            {
+                output += args.Data + breakLine;
+            };
 
-            p.WaitForExit(TimeSpan.FromMinutes(1).Milliseconds); //等待程序执行完退出进程
+            p.ErrorDataReceived += (sender, args) =>
+            {
+                output += args.Data + breakLine;
+            };
+
+            p.BeginOutputReadLine();
+            p.BeginErrorReadLine();
+
+            // 如果应用程序没有退出，那么 ReadToEnd 将会等待，也就是当前进程将会等待这个进程退出
+            ////获取输出信息
+            //string output = p.StandardOutput.ReadToEnd();
+            //output += p.StandardError.ReadToEnd();
+
+            // 等待程序执行完退出进程
+            p.WaitForExit(TimeSpan.FromMinutes(1).Milliseconds); 
+
+            // 第二次等待解决 OutputDataReceived 调用的坑
+            // 请看官方代码 https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.process.outputdatareceived?view=netcore-3.1
+            p.WaitForExit(); 
             p.Close();
 
-            return output + "\r\n";
+            return output + breakLine;
         }
     }
 }
