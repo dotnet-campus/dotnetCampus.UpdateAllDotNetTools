46b1d9760a309e1fcae6fe93f9cf8e3cababc97a
lindexi <lindexi_gd@163.com>
2020-08-10 08:52:49 +0800

    Add more output

--- src/Program.cs
+++ src/Program.cs
@@ -62,7 +62,11 @@ namespace dotnetCampus.UpdateAllDotNetTools
                 workingDirectory = Environment.CurrentDirectory;
             }
 
-            var p = new Process
+            Console.WriteLine($"FileName={fileName}");
+            Console.WriteLine($"Argument={argument}");
+            Console.WriteLine($"WorkingDirectory={workingDirectory}");
+
+            var process = new Process
             {
                 StartInfo =
                 {
@@ -79,22 +83,22 @@ namespace dotnetCampus.UpdateAllDotNetTools
                 }
             };
 
-            p.Start(); //启动程序
+            process.Start(); //启动程序
 
             const string breakLine = "\r\n";
             string output = "";
-            p.OutputDataReceived += (sender, args) =>
+            process.OutputDataReceived += (sender, args) =>
             {
                 output += args.Data + breakLine;
             };
 
-            p.ErrorDataReceived += (sender, args) =>
+            process.ErrorDataReceived += (sender, args) =>
             {
                 output += args.Data + breakLine;
             };
 
-            p.BeginOutputReadLine();
-            p.BeginErrorReadLine();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
 
             // 如果应用程序没有退出，那么 ReadToEnd 将会等待，也就是当前进程将会等待这个进程退出
             ////获取输出信息
@@ -102,12 +106,12 @@ namespace dotnetCampus.UpdateAllDotNetTools
             //output += p.StandardError.ReadToEnd();
 
             // 等待程序执行完退出进程
-            p.WaitForExit(TimeSpan.FromMinutes(1).Milliseconds); 
+            process.WaitForExit(TimeSpan.FromMinutes(1).Milliseconds); 
 
             // 第二次等待解决 OutputDataReceived 调用的坑
             // 请看官方代码 https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.process.outputdatareceived?view=netcore-3.1
-            p.WaitForExit(); 
-            p.Close();
+            process.WaitForExit(); 
+            process.Close();
 
             return output + breakLine;
         }
